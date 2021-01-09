b5420cad0f215e0640de2a3181399fd7dd52ce00
zekailin00 <46849761+zekailin00@users.noreply.github.com>
2021-01-09 23:26:58 +0800

    update

--- submit-ray-gpu-v2.sh
+++ submit-ray-gpu-v2.sh
@@ -5,11 +5,10 @@
 
 
 #SBATCH -C gpu -J Raytune -t 3:59:00 --account m2043
-#SBATCH -N2 -c 10 --ntasks-per-node=3 -G 10
+#SBATCH -N2 -c 10 --ntasks-per-node=3 -G 7
 
 
-echo GPU per Ray work nodes: ${SLURM_GPUS}
-echo Total number of work nodes: ${SLURM_NTASKS}
+echo Total number of GPU: ${SLURM_GPUS}
 
 suffix='6379'
 ip_head=`hostname`:$suffix
@@ -22,4 +21,4 @@ sleep 10
 srun --nodes=1 --ntasks=1 ray start --address $ip_head --block &
 sleep 5
 
-python ./train_RayTune.py --dataPath /global/homes/b/balewski/prjn/neuronBBP-pack40kHzDisc/probe_quad/bbp153 --probeType quad -t 13500 --useDataFrac 1 --steps 10 --rayResult $SCRATCH/ray_results --numHparams 30
+python ./train_RayTune.py --dataPath /global/homes/b/balewski/prjn/neuronBBP-pack40kHzDisc/probe_quad/bbp153 --probeType quad -t 8000 --useDataFrac 1 --steps 10 --rayResult $SCRATCH/ray_results --numHparams 20
